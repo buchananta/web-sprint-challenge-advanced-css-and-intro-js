40207beec493e76b4e6e620c430dc4400fcf4391
Trevor Buchanan <trevor@trevorbuchanan.com>
2020-07-17 10:49:36 -0500

    finished a few js tasks for task 2b

--- index.js
+++ index.js
@@ -208,10 +208,13 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
-
+console.log(artists[0].name);
+console.log(artists[2].bio);
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+artists[8].name = "Vincent Van Dough";
+console.log(artists[8]);
 
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
@@ -223,18 +226,23 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+  return `The artist at index ${index} is ${array[index].name}.`
   }
   
+console.log(getArtistByIndex(artists, 0));
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
-
-  /* Code here */
-
+function get20s(artists){
+  //no .filter I guess
+  let result = [];
+  for (let i = 0; i < artists.length; i++) {
+    let y = artists[i].years.split(' - '); // y short, for the bajillion boolean tests
+    if (y[0] < 2000 && y[0] > 1899 && y[1] < 2000 && y[1] > 1899)
+      result.push(artists[i].name);
+  return result;
 }
 
 
